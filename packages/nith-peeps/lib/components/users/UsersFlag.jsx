4883ede585836c88b69c48436d7db32950d00999
gdad-s-river <arihantverma1994@gmail.com>
2017-06-16 10:01:05 +0530

    (added yarn error file in gitignore) + (better message handling for visible flag information to various users)

--- packages/nith-peeps/lib/components/users/UsersFlag.jsx
+++ packages/nith-peeps/lib/components/users/UsersFlag.jsx
@@ -4,6 +4,21 @@ import PropTypes from "prop-types";
 import classNames from "classnames";
 import { withFlag, hasFlagged } from "meteor/nith:peeps-flagging";
 import { /*FormattedMessage,*/ intlShape } from "meteor/vulcan:i18n";
+import glamorous from "glamorous";
+
+const FlagContainer = glamorous.div({
+	borderTop: "2px solid black",
+	padding: "10px 0",
+});
+
+const FlagButton = glamorous.button({
+	padding: "10px",
+	border: "none",
+	color: "white",
+	background: "red",
+	margin: "0 10px",
+	cursor: "pointer",
+});
 
 class UsersFlag extends PureComponent {
 	flagHandler = e => {
@@ -14,7 +29,27 @@ class UsersFlag extends PureComponent {
 	};
 
 	render() {
-		return <button onClick={this.flagHandler}>Flag</button>;
+		const { userToFlagDoc, flagAction, currentUser } = this.props;
+
+		return (
+			<FlagContainer>
+				{userToFlagDoc.flagVotes
+					? <span
+							style={{ marginBottom: "10px", display: "inline-block" }}
+						>{`This User Has Been Flagged ${userToFlagDoc.flagVotes} Times`}</span>
+					: null}
+
+				{_.findWhere(userToFlagDoc.flagVoters, { _id: currentUser._id })
+					? <div style={{ padding: "10px 0" }}>You've flagged this user</div>
+					: <div>
+							<span>Not an NITian?</span>
+							<FlagButton onClick={this.flagHandler}>
+								Flag This Person
+							</FlagButton>
+						</div>}
+
+			</FlagContainer>
+		);
 	}
 }
 
