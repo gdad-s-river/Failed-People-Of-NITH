42140711ba21c5f125df6262d381071ea4e867d3
gdad-s-river <arihantverma1994@gmail.com>
2017-07-01 13:08:54 +0530

    added /flagged route in routes file + 100% container fix (did a 100% to a new react div that flung out of nowhere, don't know how)

--- packages/nith-peeps/lib/modules/routes.js
+++ packages/nith-peeps/lib/modules/routes.js
@@ -6,11 +6,13 @@ import UsersAccount from '../components/users/UsersAccount.jsx';
 import UsersSingle from '../components/users/UsersSingle.jsx';
 import Search from '../components/Search.jsx';
 import UsersSearchList from '../components/UsersSearchList.jsx'
+import UsersFlaggedAdminList from '../components/users/UsersFlaggedAdminList.jsx';
 
 addRoute([
   { name: 'home', path: '/', component: LoginScreen },
   { name: 'complete.profile', path: "/complete-profile", component: UsersProfileCheckWrapper },
   { name:'users.edit', path:'users/:slug/edit', component: UsersAccount },
   { name:'users.single', path:'users/:slug', component: UsersSingle },
-  { name:'users.search', path:'/search', component: UsersSearchList }
+  { name:'users.search', path:'/search', component: UsersSearchList },
+  {name: 'users.flagged', path: "/flagged", component: UsersFlaggedAdminList}
 ]);
